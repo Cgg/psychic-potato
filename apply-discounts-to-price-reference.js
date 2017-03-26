84bb26f206f89edb8552cfef18803d352dd62a31
Cgg <clement.geiger@gmail.com>
2017-03-27 00:51:35 +0100

    add a function to apply discounts to a price reference

---
+++ apply-discounts-to-price-reference.js
@@ -0,0 +1,36 @@
+/**
+ * Apply discounts to a price reference, return a new price reference with
+ * discounted prices.
+ *
+ * @param {Object} priceRef
+ * @param {Object} discountMap as built by makeDiscountMap
+ *
+ * @return {Object} a new price reference with discounted prices
+ */
+function applyDiscountsToPriceReference (priceRef, discountMap) {
+  var result = Object.assign({}, priceRef);
+
+  Object.keys(discountMap).forEach(function (id) {
+    if (!discountMap.hasOwnProperty(id) ||
+        !result.hasOwnProperty(id)) {
+      return;
+    }
+
+    discount = discountMap[id];
+
+    switch (discount.type) {
+      case 'percentage':
+        result[id] *=  (discount.value / 100);
+        break;
+      case 'amount':
+        result[id] -= discount.value;
+        break;
+    }
+
+  });
+
+  return result;
+}
+
+module.exports = applyDiscountsToPriceReference;
+
---
+++ test/apply-discounts-to-price-reference.t.js
@@ -0,0 +1,33 @@
+const applyDiscountsToPriceReference = require('../apply-discounts-to-price-reference.js');
+
+var tests = {};
+
+tests['apply percentage discounts'] = function (T) {
+  var discount = {
+    1: {type: 'percentage', value: 50}
+  };
+
+  var priceReference = {
+    1: 100
+  };
+
+  T.assert.deepEqual(
+      applyDiscountsToPriceReference(priceReference, discount),
+      {1: 50});
+};
+
+tests['apply amount discounts'] = function (T) {
+  var discount = {
+    1: {type: 'amount', value: 20}
+  };
+
+  var priceReference = {
+    1: 100
+  };
+
+  T.assert.deepEqual(
+      applyDiscountsToPriceReference(priceReference, discount),
+      {1: 80});
+};
+
+module.exports = tests;
