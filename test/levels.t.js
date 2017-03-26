776e13b1dcc3afafbf96b35e8bb1cb06a52f1a76
Cgg <clement.geiger@gmail.com>
2017-03-26 23:52:47 +0100

    move level tests in test/levels.t.js

---
+++ test/levels.t.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const level1 = require('../level1/level1.js');
+
+function pass (levelDir, T) {
+  const outputPath = path.join(__dirname, '..', levelDir, 'output.json');
+  const dataPath = path.join(__dirname, '..', levelDir, 'data.json');
+
+  const levelFunction = require(path.join(__dirname, '..', levelDir, levelDir + '.js'));
+
+  T.assert.deepEqual(
+    JSON.stringify(JSON.parse(fs.readFileSync(outputPath))),
+    levelFunction(fs.readFileSync(dataPath))
+  );
+}
+
+var tests = {};
+
+tests['LEVEL1 PASS'] = function (T) {
+  pass('level1', T);
+};
+
+module.exports = tests;
+
